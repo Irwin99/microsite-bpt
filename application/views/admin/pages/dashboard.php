6db20b0e69b8a85d41924ff775bef918afb746e1
Irwin99 <hakimirwin@gmail.com>
2016-12-01 14:43:34 +0900

    comit3
    asd

--- application/views/admin/pages/dashboard.php
+++ application/views/admin/pages/dashboard.php
@@ -11,7 +11,7 @@
 ?>
 
 <!-- jQuery 2.2.3 -->
-<script src="asset/asset_lte/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="<?php echo base_url('asset/asset_lte/plugins/jQuery/jquery-2.2.3.min.js')?>"></script>
 <!-- jQuery UI 1.11.4 -->
 <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
@@ -19,14 +19,14 @@
   $.widget.bridge('uibutton', $.ui.button);
 </script>
 <!-- Bootstrap 3.3.6 -->
-<script src="asset/asset_lte/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url('asset/asset_lte/bootstrap/js/bootstrap.min.js')?>"></script>
 <!-- Morris.js charts -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="asset/asset_lte/plugins/morris/morris.min.js"></script>
+<script src="<?php echo base_url('asset/asset_lte/plugins/morris/morris.min.js')?>"></script>
 <!-- Sparkline -->
-<script src="asset/asset_lte/plugins/sparkline/jquery.sparkline.min.js"></script>
+<script src="<?php echo base_url('asset/asset_lte/plugins/sparkline/jquery.sparkline.min.js')?>"></script>
 <!-- jvectormap -->
-<script src="asset/asset_lte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="<?php echo base_url('asset/asset_lte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')?>"></script>
 <script src="asset/asset_lte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
 <!-- jQuery Knob Chart -->
 <script src="asset/asset_lte/plugins/knob/jquery.knob.js"></script>
